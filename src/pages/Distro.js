782198d4b0eebd617889d0277fd9e71e5627307d
unknown <ianmmah@gmail.com>
2022-10-01 23:52:11 -0700

    Generate new QR codes

--- src/pages/Distro.js
+++ src/pages/Distro.js
@@ -7,27 +7,48 @@ import { Link } from 'react-router-dom'
 import Button from '../components/Button'
 import Card from '../components/Card'
 import LensQR from '../components/LensQR'
+import { HOSTNAME } from '../utils/constants'
 import { collection, addDoc } from 'firebase/firestore'
 
 const Container = styled.div`
-    border-radius: 8px;`
+    border-radius: 8px;
+    text-align: center;
+`
 
 function Home({ profile, db, ...props }) {
     const { wallet, provider } = useWallet()
+    const [nextUrl, setNextUrl] = useState('')
+
+    const genCode = async () => {
+      try {
+        const docRef = await addDoc(collection(db, "codes"), {})
+        console.log("Document written with ID: ", docRef.id)
+        const url = `${HOSTNAME}/scan/${docRef.id}`
+        console.log(url)
+        setNextUrl(url)
+      } catch (e) {
+        console.error("Error adding document: ", e)
+      }
+    }
+
+    useEffect(() => {
+      if (!nextUrl) {
+        genCode()
+      }
+    })
     
     const handleClick = async () => {
-
-        try {
-          const docRef = await addDoc(collection(db, "codes"), {});
-          console.log("Document written with ID: ", docRef.id);
-        } catch (e) {
-          console.error("Error adding document: ", e);
-        }
-        
+      await genCode()
     }
 
     return <Container>
-        <LensQR link={"https://irisapp.xyz/"}/>
+        
+        <br/>
+        <a href={nextUrl} target="_blank" rel="noreferrer">
+          <LensQR link={nextUrl}/>
+          {nextUrl}
+        </a>
+        <br/>
         <Button onClick={handleClick}>Refresh code</Button>
     </Container>
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,4 @@ export const ADMIN_LIST = [
     '0x374002dD7f555D037F7566cC75ce563765a2F456',
 
 ]
+export const HOSTNAME = process.env.NODE_ENV === 'production' ? 'https://lens-wl.web.app' : 'http://localhost:4783'
