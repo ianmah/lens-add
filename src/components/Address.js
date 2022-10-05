91070444a2a4b221e214183167962bf10b831a3c
unknown <ianmmah@gmail.com>
2022-10-05 01:35:14 -0700

    signout ux

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { useAccount, useDisconnect } from 'wagmi'
+import signout from '../assets/signout.svg'
 
 const Span = styled.div`
     background: #74434A;
@@ -8,9 +9,12 @@ const Span = styled.div`
     border-radius: 10px;
     margin: 1em;
     box-sizing: border-box;
-    max-width: 9em;
+    max-width: 13em;
     position: absolute;
     right: 0;
+    display: flex;
+    align-items: center;
+    gap: 4px;
 `;
 
 const Address = ({ }) => {
@@ -20,7 +24,8 @@ const Address = ({ }) => {
     return <>
         { isConnected && 
             <Span onClick={() => disconnect()}>
-            {address.substring(0, 6)}...{address.substring(37, 41)}
+                {address.substring(0, 6)}...{address.substring(37, 41)}
+                <img src={signout} alt='signout' width='16px' style={{ display: 'inline' }} />
             </Span>
         }       
     </>
